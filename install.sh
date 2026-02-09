30caa224224c412c3ad2f19d368b8c89a3855dba
xcalibur839 <9098856+xcalibur839@users.noreply.github.com>
2026-02-09 04:27:36 -0800

    Improve cross platform compatibility

--- install.sh
+++ install.sh
@@ -2,6 +2,36 @@
 
 source /etc/os-release
 
+# Create needed directories
+mkdir -p bin keys
+
+export DSi="false"
+
+# Install extras if available, exit otherwise
+if [ -e extras.sh ]; then
+    ./extras.sh
+elif [ -e keys/biosnds7 -a -e keys/biosdsi7.bin ]; then
+    if [ ! -e bin/dsimode.nds ]; then
+        echo "WRFUTester v0.60 (dsimode.nds) not found in the bin folder. Press Ctrl+C to cancel, or"
+        echo "Press Enter to continue"
+        read
+    else
+        echo "Using manually downloaded files (with full DSi support)"
+        export DSi="true"
+    fi
+else
+    echo
+    echo
+    echo Required files missing:
+    echo keys/biosnds7.bin, keys/biosdsi7.bin
+    echo
+    echo Optional DSi file missing:
+    echo "bin/dsimode.nds (WRFUTester v0.60)"
+    echo
+    echo
+    exit 1
+fi
+
 if [ -d platforms/${ID} ]; then
     export base_dir=$(dirname $(realpath "$0"))/bin
     platforms/${ID}/dependencies.sh
--- platforms/ubuntu/dependencies.sh
+++ platforms/ubuntu/dependencies.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-source ./config.sh
-
 # Install dependencies
 sudo add-apt-repository -y ppa:dotnet/backports
 sudo apt update && sudo apt install -y wget dotnet-sdk-9.0 cmake gcc-arm-none-eabi build-essential git python3 7zip
