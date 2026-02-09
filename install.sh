76718ec2b04b93ebea47e8957b05d5576c9a55b3
xcalibur839 <9098856+xcalibur839@users.noreply.github.com>
2026-02-09 05:10:38 -0800

    Fix missing file extension

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ export DSi="false"
 # Install extras if available, exit otherwise
 if [ -e extras.sh ]; then
     ./extras.sh
-elif [ -e keys/biosnds7 -a -e keys/biosdsi7.bin ]; then
+elif [ -e keys/biosnds7.bin -a -e keys/biosdsi7.bin ]; then
     if [ ! -e bin/dsimode.nds ]; then
         echo "WRFUTester v0.60 (dsimode.nds) not found in the bin folder. Press Ctrl+C to cancel, or"
         echo "Press Enter to continue"
